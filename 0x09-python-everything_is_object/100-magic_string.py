f431cbc85b41edfbf33971d6d972b2f1a68c2513
Madalitso Kalua <kaluamadalitso@yahoo.com>
2021-12-15 21:26:29 +0200

    my commits

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
-    return "Holberton" + ", Holberton" * getattr(magic_string, "x", 0)
+    magic_string.ntimes = getattr(magic_string, 'ntimes', 0) + 1
+    return ("Holberton, " * (magic_string.ntimes - 1) + "Holberton")
