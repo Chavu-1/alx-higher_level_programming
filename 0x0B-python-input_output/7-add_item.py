90d2c851fac2e4daca5d4b377c6af12372f48f61
Madalitso Kalua <kaluamadalitso@yahoo.com>
2021-12-21 20:59:02 +0200

    Python - Input/Output correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-'''Module for saving argv info via json to file.'''
-import json
-import os.path
-import sys
+"""
+Module 9-add_item
+Contains function that adds and saves to Python obj to JSON file; loads objects
+# run with ./9-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./9-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
+"""
+
+
+from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
+filename = "add_item.json"
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
 
-save_to_json_file(json_list, file)
+save_to_json_file(existing_content + argv[1:], filename)
